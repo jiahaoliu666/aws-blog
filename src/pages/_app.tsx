b0263125c92c90d60ae67062c8a24786234295d2
jiahaoliu666 <jiahaoliu@metaage.com.tw>
2024-10-20 23:07:25 +0800

    new

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,13 +4,17 @@ import { AppProps } from 'next/app';
 import { AuthProvider } from '../context/AuthContext';
 import { AppProvider } from '../context/AppContext';
 import { LanguageProvider } from '../context/LanguageContext'; // 引入 LanguageProvider
+import Head from 'next/head';
 import '../styles/globals.css'; // 導入全局樣式
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <AuthProvider>
       <AppProvider>
-        <LanguageProvider> {/* 將 LanguageProvider 包裹在這裡 */}
+        <LanguageProvider>
+          <Head>
+            <title>AWS Blog</title> {/* 在這裡添加標題 */}
+          </Head>
           <Component {...pageProps} />
         </LanguageProvider>
       </AppProvider>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,18 +4,31 @@ import Link from 'next/link';
 import Navbar from '../components/common/Navbar'; // 確保正確導入 Navbar  
 import Footer from '../components/common/Footer'; // 引入 Footer
 import '@aws-amplify/ui-react/styles.css'; // 確保樣式被正確導入
+import { useEffect } from 'react'; // 引入 useEffect
 
 const Home: React.FC = () => {
+  useEffect(() => {
+    const elements = document.querySelectorAll('.fade-in');
+    elements.forEach((el) => {
+      el.classList.add('opacity-0');
+      setTimeout(() => {
+        el.classList.add('transition-opacity', 'duration-1000', 'opacity-100');
+      }, 100);
+    });
+  }, []);
+
   return (
-    <div className="flex flex-col min-h-screen bg-gray-100">
+    <div className="flex flex-col min-h-screen bg-gradient-to-b from-gray-100 to-gray-300">
       <Navbar />
-      <div className="flex flex-col items-center justify-center flex-grow px-4 py-8">
-        <h1 className="text-5xl font-extrabold text-center mb-6 text-gray-900 drop-shadow-lg">歡迎來到 AWS Blog</h1>
+      <div className="flex flex-col items-center justify-center flex-grow px-4 py-8 fade-in">
+        <h1 className="text-5xl font-extrabold text-center mb-6 text-gray-900 drop-shadow-lg">
+          歡迎來到 <span className="text-blue-600">AWS Blog</span>
+        </h1>
         <p className="text-xl text-center mb-6 text-gray-800 max-w-2xl leading-relaxed">
           這是一個專為 AWS 文章而設的閱讀平台。在這裡，您可以輕鬆找到、收藏和管理您的文章。
         </p>
         <Link href="/news" legacyBehavior>
-          <a className="bg-blue-600 text-white px-6 py-3 rounded-full shadow-lg hover:shadow-xl transition duration-300 transform hover:scale-105">
+          <a className="bg-blue-600 text-white px-6 py-3 rounded-full shadow-lg hover:shadow-xl transition duration-300 transform hover:scale-105 hover:bg-blue-700">
             開始閱讀文章
           </a>
         </Link>
@@ -35,3 +48,4 @@ const Home: React.FC = () => {
 };
 
 export default Home;
+
