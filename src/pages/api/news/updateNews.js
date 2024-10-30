40e89800a3452836be53f14f4727a6398e196d4c
jiahaoliu666 <jiahaoliu@metaage.com.tw>
2024-10-30 22:03:35 +0800

    測試

--- src/pages/api/news/updateNews.js
+++ src/pages/api/news/updateNews.js
@@ -36,14 +36,14 @@ async function getNewArticles() {
   return {
     articles: latestItems.map((item) => ({
       content: `
-        <div class="flex items-center">
+        <div class="flex items-center"></div>
           ${
             item.read
               ? ""
               : '<span class="inline-block w-2 h-2 bg-blue-500 rounded-full mr-2"></span>'
           }
           <div class="flex-1">
-            <a href="/news" class="text-blue-600 hover:text-blue-800 hover:underline transition duration-150">[最新新聞]</a> 有新的文章：
+            <a href="/news" class="text-blue-600 hover:text-blue-800 hover:underline transition duration-150">[最新新聞]</a> 有新的文章！<br>
             <a href="${
               item.link.S
             }" class="text-blue-600 hover:text-blue-800 hover:underline transition duration-150" target="_blank">${
