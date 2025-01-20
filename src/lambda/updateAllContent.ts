6e756aeb859be8749e4b32023ce72ca3992ed7ce
Jiahao <qaz7416@gmail.com>
2025-01-20 21:48:39 +0800

    1

--- src/lambda/updateAllContent.ts
+++ src/lambda/updateAllContent.ts
@@ -60,8 +60,8 @@ dotenv.config({ path: ".env.local" });
 
 // 常量定義
 const FETCH_COUNTS = {
-  announcement: 6, // 更新公告數量
-  news: 0, // 更新新聞數量
+  announcement: 0, // 更新公告數量
+  news: 2, // 更新新聞數量
   solutions: 0, // 更新解決方案數量
   architecture: 0, // 更新架構數量
   knowledge: 0, // 更新知識中心數量
